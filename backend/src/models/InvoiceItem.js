c962995884e102dbab8e8715bb735fce1089fb10
jimenezmedinarobinson <jimenezmedinarobinson@gmail.com>
2026-06-11 16:11:01 -0500

    Add InvoiceItem model

---
+++ backend/src/models/InvoiceItem.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+
+const InvoiceItem = sequelize.define('InvoiceItem', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  invoiceId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  productId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  unitPrice: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  subtotal: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+  tableName: 'invoice_items',
+});
+
+module.exports = InvoiceItem;
